5ad0161664e15ee0f1c8cc2a298db34763a18fc9
JOlCode <61011693+JOlCode@users.noreply.github.com>
2025-01-15 15:13:25 +0100

    updated paths etc in make_clarabel.m; removed .dll and .mex64

--- make_clarabel.m
+++ make_clarabel.m
@@ -1,12 +1,12 @@
 clc
 clear
 
-% Define the source path of the file
-sourceFolder = 'C:\path\to\source\folder'; % Replace with the actual path
-fileName = 'clarabel_c.lib';
-
 % Define the current folder as the destination
-destinationFolder = pwd; % 'pwd' gives the current working directory
+destinationFolder = pwd; 
+
+% Define the source path of the file
+sourceFolder = '..\..\Clarabel\clarabel\rust_wrapper\target\release'; % Replace with the actual path
+fileName = 'clarabel_c.dll';
 
 % Create the full path for the source file and the destination
 sourceFile = fullfile(sourceFolder, fileName);
@@ -18,11 +18,60 @@ copyfile(sourceFile, destinationFile);
 % Display a success message
 disp(['File copied to: ', destinationFile]);
 
+% just for display output
+pause(1)
+disp('');
+disp('');
 
-% generate clarabel mex interface
-mex -v clarabel_mex.cpp ...
-        -DFEATURE_SDP ...       % activate SDP feature
-      '-I./eigen' ...
-       -I"C:\Users\ac133867\Desktop\clarabel_mex_beta\clarabel_mex\clarabel\include" ...
-         "C:\Users\ac133867\Desktop\clarabel_mex_beta\clarabel_mex\clarabel\clarabel_c.dll.lib" ...
-         "C:\Users\ac133867\Desktop\clarabel_mex_beta\clarabel_mex\clarabel\clarabel_c.lib"
+% Set to true to include -DFEATURE_SDP if you want to use the SDP feature
+% IMPORTANT: libraries must be compiled with the sdp-feature!
+useFeatureSDP = true;     
+
+% Get eigen path from environment variable or change the folder where you
+% stored eigen
+eigenPath = getenv('EIGEN_PATH'); 
+if isempty(eigenPath)
+    error('Environment variable EIGEN_PATH is not set.');
+else
+    disp(['Eigen path is: ', eigenPath]);
+end
+
+% just for display output
+disp('');
+disp('');
+
+pause(1)
+
+% Common paths to clarabel; make sure you first compiled the libraries!
+clarabelIncludePath = '..\..\Clarabel\clarabel\include';
+clarabelDLLPath = '..\..\Clarabel\clarabel\rust_wrapper\target\release\clarabel_c.dll.lib';
+clarabelLibPath = '..\..\Clarabel\clarabel\rust_wrapper\target\release\clarabel_c.lib';
+
+% Construct the mex command
+mexCmd = [
+    'mex -v clarabel_mex.cpp ' ...
+    (useFeatureSDP * '-DFEATURE_SDP ') ... % Conditionally add -DFEATURE_SDP
+    '-I"' eigenPath '" ' ... 
+    '-I"' clarabelIncludePath '" ' ...
+    '"' clarabelDLLPath '" ' ...
+    '"' clarabelLibPath '"'
+];
+
+%% Execute the mex command
+disp('Executing mex command:');
+disp(mexCmd);
+eval(mexCmd); % Run the constructed mex command
+
+disp('');
+disp('');
+
+pause(1)
+
+%% add Clarabel_matlab path and subfolder permanently to matlab path
+disp('Adding clarabel_matlab folder and subfolder permanently to your MATLAB path.');
+folderToAdd = '../';
+addpath(genpath(folderToAdd));
+savepath;
+
+
+disp('Clarabel .mex generation done.');
